d61af96fa854e22560c8ad6a408d2e1d5a14faf6
Geek_cat <by6dx1@gmail.com>
2020-07-14 23:13:00 +0800

    2020-07-14 23:13

--- Auto-git.sh
+++ Auto-git.sh
@@ -32,7 +32,7 @@ cd $1
 note="$(date +%Y-%m-%d) $(date +%H:%M)"
 git add -A
 git commit -m "$note"
-if [ $? == 0 ];then
+if [ $?==0 ]; then
 	echo "Finish commit"
 else 
 	echo "Error with this commit!"
